88e1f16f218cf15596eebc042b4c0ea183ff2c87
Nicholas Lantz <nlantz@pm.me>
2020-01-30 13:44:49 -0700

    Added templates stub

---
+++ templates.lisp
@@ -0,0 +1,6 @@
+(defpackage :templates
+  (:use :cl :bibliotheca))
+(in-package :templates)
+
+;; TODO: Most functionality should be handled by Lisp and the
+;; XML library.  Additional stuff should go here.
