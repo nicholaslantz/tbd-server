e595f38150d6e56b8e04b1c913dbd5cab2fc5396
Nicholas Lantz <nlantz@pm.me>
2020-02-16 12:50:13 -0700

    Changed DATETIME to output GMT instead of local time.

--- date.lisp
+++ date.lisp
@@ -10,12 +10,19 @@
   #(Jan Feb Mar Apr May Jun
     Jul Aug Sep Oct Nov Dec))
 
+(defparameter *timezone*
+  (~>> (get-universal-time)
+    (decode-universal-time)
+    (multiple-value-list)
+    (nth 8)))
+
 (defun datetime (&optional (universal-time (get-universal-time)))
-  (multiple-value-bind
-	(sec min hr date month year weekday)
-      (decode-universal-time universal-time)
-    (format nil
-	    "~:(~A~), ~D ~:(~A~) ~D ~2,'0D:~2,'0D:~2,'0D"
-	    (aref *days-of-week* weekday)
-	    date (aref *months* (- month 1)) year
-	    hr min sec)))
+  (let ((utc-time (+ universal-time (* *timezone* 60 60))))
+    (multiple-value-bind
+	  (sec min hr date month year weekday)
+	(decode-universal-time utc-time)
+      (format nil
+	      "~:(~A~), ~D ~:(~A~) ~D ~2,'0D:~2,'0D:~2,'0D GMT"
+	      (aref *days-of-week* weekday)
+	      date (aref *months* (- month 1)) year
+	      hr min sec))))
